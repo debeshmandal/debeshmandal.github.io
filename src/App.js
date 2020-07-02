1375d628981b4761af2c2148c495b4aa08db9e5f
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-02 15:28:44 +0100

    multi-page with decent design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header'
+import Footer from './components/footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { Route } from "react-router-dom";
+
+import Home from './components/home';
+import CV from './components/cv';
+
+function App(props) {
+  return ( 
+    <div>
+      <Header />
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/cv" component={CV} />
+      <Footer />
+    </div>);
 }
 
 export default App;
