de8647df7ddc9bc4cfa0e76bfdc5bf9079324f2d
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-02 16:28:24 +0100

    ready to deploy

---
+++ src/components/games.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/games.css';
+function Games () {
+    return <div className='App-main'><p>To be confirmed - some fun and games</p></div>
+};
+export default Games;
