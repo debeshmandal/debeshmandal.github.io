35e6e54dd65a7e472438e39b41103e80e5fce4a7
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2021-06-08 19:30:14 +0100

    created frame component

---
+++ src/components/frame.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Frame () {
+    return (
+        <div className='frame'>
+            <img src="https://via.placeholder.com/150" alt="placeholder"/>
+        </div>
+    )
+};
+export default Frame;
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Frame from './frame'
 function Home () {
-    return <div className='App-main'></div>
+  return (
+    <div className='App-main'>
+      <Frame />
+    </div>
+  )
 };
 export default Home;
